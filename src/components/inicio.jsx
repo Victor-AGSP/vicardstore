b4629095758a6d03ad6e662c460c9c314ea9da37
Victor-AGSP <victorsepulveda.agp@gmail.com>
2025-05-28 23:09:07 -0400

    Correccion

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -12,8 +12,8 @@ function Inicio() {
         <p>Explora todo lo relacionado a Yu-Gi-Oh!</p>
       </div>
       <div className="section-card" onClick={() => navigate('/mylpb')}>
-        <h2>Magic: The Gathering</h2>
-        <p>Explora todo lo relacionado a Magic</p>
+        <h2>Mitos y Leyendas PB</h2>
+        <p>Explora todo lo relacionado a MyL</p>
       </div>
     </div>
   );
